f738cfdbaf045bc2a1390a48696d682699dce38a
1felipeaac <felipeaacoelho@gmail.com>
2023-04-25 13:44:09 -0300

    project structure

---
+++ src/routes/users.routes.js
@@ -0,0 +1,11 @@
+const { Router } = require ("express");
+
+const UsersController = require("../controllers/UserController")
+
+const userRoutes = Router();
+
+const usersController = new UsersController()
+
+userRoutes.post("/", usersController.create);
+
+module.exports = userRoutes;
